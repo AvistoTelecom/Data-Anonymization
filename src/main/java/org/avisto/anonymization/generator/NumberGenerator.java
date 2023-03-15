f26843215b2ca308f007c24f68c28a245a664f57
Zack de Saint Pern <zack.de-saint-pern@avisto.com>
2023-03-15 12:05:26 +0100

    refactor of NumberGenerator

--- src/main/java/org/avisto/anonymization/generator/NumberGenerator.java
+++ src/main/java/org/avisto/anonymization/generator/NumberGenerator.java
@@ -1,10 +1,6 @@
 package org.avisto.anonymization.generator;
 
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
@@ -44,33 +40,6 @@ public final class NumberGenerator {
         return ThreadLocalRandom.current().nextFloat() * (max - min) + min;
     }
 
-    /**
-     * Generate a Stream of size {size} random Float, values are between [ -Float.MAX_VALUE, Float.MAX_VALUE [
-     * @param size the number of Float to generate
-     * @return Stream of {size} element  of random Float in [ -Float.MAX_VALUE, Float.MAX_VALUE [
-     */
-    public static Stream<Float> generateFloats(Integer size) {
-        Float[] res = new Float[size];
-        for (int i = 0; i < size; i++) {
-            res[i] = generateFloat();
-        }
-        return Arrays.stream(res);
-    }
-
-    /**
-     * Generate a Stream of size {size} random Float, values are between [ min, max [
-     * @param size the number of Float to generate
-     * @param min min value
-     * @param max max value
-     * @return Stream of {size} element  of random Float in [ min, max [
-     */
-    public static Stream<Float> generateFloats(Integer size, Float min, Float max) {
-        Float[] res = new Float[size];
-        for (int i = 0; i < size; i++) {
-            res[i] = generateFloat(min, max);
-        }
-        return Arrays.stream(res);
-    }
 
     /**
      * Generate a random Double, the value is between [ -Double.MAX_VALUE, Double.MAX_VALUE [
@@ -93,33 +62,7 @@ public final class NumberGenerator {
         return min + ThreadLocalRandom.current().nextDouble() * (max - min);
     }
 
-    /**
-     * Generate a Stream of size {size} random Double, values are between [ -Double.MAX_VALUE, Double.MAX_VALUE [
-     * @param size the number of Double to generate
-     * @return Stream of {size} element  of random Double in [ -Double.MAX_VALUE, Double.MAX_VALUE [
-     */
-    public static Stream<Double> generateDoubles(Integer size) {
-        Double[] res = new Double[size];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = generateDouble();
-        }
-        return Arrays.stream(res);
-    }
 
-    /**
-     * Generate a stream of size {size} random Double, values are between [ min, max [
-     * @param size the number of Double to generate
-     * @param min min value
-     * @param max max value
-     * @return Stream of {size} element  of random Double in [ min, max [
-     */
-    public static Stream<Double> generateDoubles(Integer size, Double min, Double max) {
-        Double[] res = new Double[size];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = generateDouble(min, max);
-        }
-        return Arrays.stream(res);
-    }
 
     /**
      * Generate a random Integer
@@ -140,30 +83,6 @@ public final class NumberGenerator {
             throw new IllegalArgumentException(MIN_GREATER_THAN_MAX);
         }
         return ThreadLocalRandom.current().nextInt(min, max);
-    }
-
-    /**
-     * Generate a Stream of size {size} random Integer
-     * @param size the number of Integer to generate
-     * @return Stream of {size} element  of random Integer
-     */
-    public static Stream<Integer> generateInts(Integer size) {
-        return generateInts(size, Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
-
-    /**
-     * Generate a Stream of size {size} random Integer, values are between [ min, max [
-     * @param size the number of Integer to generate
-     * @param min min value
-     * @param max max value
-     * @return Stream of {size} element  of random Integer in [ min, max [
-     */
-    public static Stream<Integer> generateInts(Integer size, Integer min, Integer max) {
-        Integer[] res = new Integer[size];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = generateInt(min, max);
-        }
-        return Arrays.stream(res);
     }
 
     /**
@@ -188,25 +107,16 @@ public final class NumberGenerator {
     }
 
     /**
-     * Generate a Stream of size {size} random Long
-     * @param size the number of Long to generate
-     * @return Stream of {size} element  of random Long
+     * generate a stream of Number <br/>
+     * the way to generate Number is define in the supplier
+     * @param size number of value
+     * @param supplier behavior of Number generation
+     * @return a stream of Number
      */
-    public static Stream<Long> generateLongs(Integer size) {
-        return generateLongs(size, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-
-    /**
-     * Generate a Stream of size {size} random Long, values are between [ min, max [
-     * @param size the number of Long to generate
-     * @param min min value
-     * @param max max value
-     * @return Stream of {size} element  of random Long in [ min, max [
-     */
-    public static Stream<Long> generateLongs(Integer size, Long min, Long max) {
-        Long[] res = new Long[size];
+    public static Stream<Number> generateNumberStream(int size, Supplier<Number> supplier) {
+        Number[] res = new Number[size];
         for (int i = 0; i < res.length; i++) {
-            res[i] = generateLong(min, max);
+            res[i] = supplier.get();
         }
         return Arrays.stream(res);
     }
