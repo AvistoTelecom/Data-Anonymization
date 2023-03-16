4a8826ce151e705f26ed2ae8b571ab7c8f235540
Zack de Saint Pern <zack.de-saint-pern@avisto.com>
2023-03-16 12:12:48 +0100

    last update for basic use

--- src/main/java/org/avisto/anonymization/annotation/RandomizeString.java
+++ src/main/java/org/avisto/anonymization/annotation/RandomizeString.java
@@ -3,12 +3,15 @@ package org.avisto.anonymization.annotation;
 import org.avisto.anonymization.generator.StringGenerator;
 import org.avisto.anonymization.model.enums.StringType;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface RandomizeString {
-    StringType type();
+    StringType value();
     int minSize() default StringGenerator.DEFAULT_MIN_SIZE;
     int maxSize() default StringGenerator.DEFAULT_MAX_SIZE;
     int minLength() default StringType.DEFAULT_LENGTH_BEHAVIOR;
