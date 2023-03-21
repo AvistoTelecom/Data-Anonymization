b7bc6d10decc16035947d0a260e1041e086f2be4
Zack de Saint Pern <zack.de-saint-pern@avisto.com>
2023-03-21 10:42:29 +0100

    update pom.xml for release version 1.1

--- src/main/java/org/avisto/anonymization/annotation/RandomizeNumber.java
+++ src/main/java/org/avisto/anonymization/annotation/RandomizeNumber.java
@@ -12,9 +12,30 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface RandomizeNumber {
 
+    /**
+     * define the Type of number to set
+     */
     NumberType value();
+
+
+    /**
+     * set the min value available. <br/>
+     * the value must fit the syntax of the NumberType selected
+     */
     String minValue() default NumberType.BEHAVIOR;
+
+    /**
+     * set the max value exclude available. <br/>
+     * the value must fit the syntax of the NumberType selected
+     */
     String maxValue() default NumberType.BEHAVIOR;
+
+    /**
+     * set the min size the Collection can have
+     */
     int minSize() default NumberGenerator.MIN_SIZE_COLLECTION;
+    /**
+     * set the max size the Collection can have
+     */
     int maxSize() default NumberGenerator.MAX_SIZE_COLLECTION;
 }
--- src/main/java/org/avisto/anonymization/annotation/RandomizeString.java
+++ src/main/java/org/avisto/anonymization/annotation/RandomizeString.java
@@ -11,12 +11,44 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface RandomizeString {
+    /**
+     * define the format of the String to set
+     */
     StringType value();
+
+    /**
+     * set the min size the Collection can have
+     */
     int minSize() default StringGenerator.DEFAULT_MIN_SIZE;
+
+    /**
+     * set the max size the Collection can have
+     */
     int maxSize() default StringGenerator.DEFAULT_MAX_SIZE;
+
+    /**
+     * set the min length the value can be
+     */
     int minLength() default StringType.DEFAULT_LENGTH_BEHAVIOR;
+
+    /**
+     * set the max length the value can be
+     */
     int maxLength() default StringType.DEFAULT_LENGTH_BEHAVIOR;
+
+    /**
+     * path of a file were each line is a possible value
+     * if STRING_FROM_FILE is used it must be defined or it wil send an exception
+     */
     String path() default "";
+
+    /**
+     * values that the field can get <br/>
+     */
     String[] possibleValues() default {};
+
+    /**
+     * pattern for the regex generator
+     */
     String pattern() default "[a-z]{5,12}";
 }
