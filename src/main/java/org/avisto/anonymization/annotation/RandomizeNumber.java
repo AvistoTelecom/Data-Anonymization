7f1684727ba538ab54c69d99f7c01fcf51162a14
Zack de Saint Pern <zack.de-saint-pern@avisto.com>
2023-04-03 15:47:12 +0200

    update javadoc

--- src/main/java/org/avisto/anonymization/annotation/RandomizeNumber.java
+++ src/main/java/org/avisto/anonymization/annotation/RandomizeNumber.java
@@ -12,28 +12,33 @@ public @interface RandomizeNumber {
 
     /**
      * define the Type of number to set
+     * @return value
      */
     NumberType value();
 
 
     /**
-     * set the min value available. <br/>
+     * set the min value available.
      * the value must fit the syntax of the NumberType selected
+     * @return value
      */
     String minValue() default NumberType.BEHAVIOR;
 
     /**
-     * set the max value exclude available. <br/>
+     * set the max value exclude available.
      * the value must fit the syntax of the NumberType selected
+     * @return value
      */
     String maxValue() default NumberType.BEHAVIOR;
 
     /**
      * set the min size the Collection can have
+     * @return value
      */
     int minSize() default NumberGenerator.MIN_SIZE_COLLECTION;
     /**
      * set the max size the Collection can have
+     * @return value
      */
     int maxSize() default NumberGenerator.MAX_SIZE_COLLECTION;
 }
